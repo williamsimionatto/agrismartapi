bb6fcff710997345c9dec0515a8da34e109a4d38
William Simionatto <william.sn0226@gmail.com>
2023-02-18 16:15:20 -0300

    chore: start Spring Boot project

---
+++ src/test/java/agrismart/com/agrismart/AgrismartApplicationTests.java
@@ -0,0 +1,13 @@
+package agrismart.com.agrismart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrismartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
