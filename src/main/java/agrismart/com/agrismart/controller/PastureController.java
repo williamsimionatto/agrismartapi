244b0b5e73c5d32b67704db81197c828726cc0d6
William Simionatto <william.sn0226@gmail.com>
2023-02-20 20:43:44 -0300

    feat: create endpoint to update pasture data

--- src/main/java/agrismart/com/agrismart/controller/PastureController.java
+++ src/main/java/agrismart/com/agrismart/controller/PastureController.java
@@ -5,12 +5,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import agrismart.com.agrismart.domain.Pasture;
 import agrismart.com.agrismart.dto.pasture.AddPastureDTO;
+import agrismart.com.agrismart.dto.pasture.EditPastureDTO;
 import agrismart.com.agrismart.service.FarmService;
 import agrismart.com.agrismart.service.PastureService;
 import agrismart.com.agrismart.service.exceptions.ObjectnotFoundException;
@@ -31,7 +33,7 @@ public class PastureController {
       if (!farmService.exists(data.getFarmId())) {
         throw new ObjectnotFoundException("Farm: " + data.getFarmId() + " not found");
       }
-  
+
       Pasture pasture = pastureService.save(data);
       return ResponseEntity.ok(pasture);
   }
@@ -45,6 +47,16 @@ public class PastureController {
   @GetMapping("/{id}")
   public ResponseEntity<Pasture> getPasture(@PathVariable Long id) {
     Pasture pasture = pastureService.getPasture(id);
-    return ResponseEntity.ok(pasture); 
+    return ResponseEntity.ok(pasture);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Pasture> updatePasture(@PathVariable Long id, @RequestBody EditPastureDTO data) {
+    if (!farmService.exists(data.getFarmId())) {
+      throw new ObjectnotFoundException("Farm: " + data.getFarmId() + " not found");
+    }
+
+    Pasture updatedPasture = pastureService.edit(id, data);
+    return ResponseEntity.ok(updatedPasture);
   }
 }
--- src/main/java/agrismart/com/agrismart/service/PastureService.java
+++ src/main/java/agrismart/com/agrismart/service/PastureService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import agrismart.com.agrismart.domain.Farm;
 import agrismart.com.agrismart.domain.Pasture;
 import agrismart.com.agrismart.dto.pasture.AddPastureDTO;
+import agrismart.com.agrismart.dto.pasture.EditPastureDTO;
 import agrismart.com.agrismart.repository.PastureRepository;
 import agrismart.com.agrismart.service.exceptions.ObjectnotFoundException;
 
@@ -32,5 +33,14 @@ public class PastureService {
 
   public Pasture getPasture(Long id) {
     return pastureRepository.findById(id).orElseThrow(() -> new ObjectnotFoundException("Pasture not found"));
+  }
+
+  public Pasture edit(Long id, EditPastureDTO data) {
+    Farm farm = farmService.getFarm(data.getFarmId());
+    Pasture pasture = getPasture(id);
+    pasture.setFarm(farm);
+    pasture.setName(data.getName());
+    pasture.setArea(data.getArea());
+    return pastureRepository.save(pasture);
   }
 }
