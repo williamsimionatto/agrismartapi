a5f790829e15f1b3585ff9a0ba956c5702af383d
William Simionatto <william.sn0226@gmail.com>
2023-02-21 13:24:02 -0300

    feat: create User entity

---
+++ src/main/java/agrismart/com/agrismart/domain/User.java
@@ -0,0 +1,46 @@
+package agrismart.com.agrismart.domain;
+
+import java.io.Serializable;
+
+import agrismart.com.agrismart.domain.enums.Role;
+import io.micrometer.common.lang.NonNull;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "user")
+public class User implements Serializable {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NonNull
+  @Column(name = "user_name", unique = true)
+  private String userName;
+
+  @NonNull
+  @Column
+  private String password;
+
+  @NonNull
+	@Column(name = "role")
+  @Enumerated(EnumType.STRING)
+	private Role role;
+
+  @Nullable
+  @Column(name = "farm_id")
+  private Long farmId;
+}
